48f698d4e2d4cc56c56583ba08158265a297633e
Betaout <Betaout@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-03 10:19:19 +0000

    contibuter change
    git-svn-id: https://plugins.svn.wordpress.org/betaout/trunk@691079 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/html/configuration.php
+++ includes/html/configuration.php
@@ -9,66 +9,85 @@ if (isset($_POST['betaoutSubmit'])) {
         $wordpressVersion = isset($_POST['wordpressVersion']) ? trim($_POST['wordpressVersion']) : '';
         $wordpressBoPluginUrl = isset($_POST['wordpressBoPluginUrl']) ? trim($_POST['wordpressBoPluginUrl']) : '';
 
-        $curlResponse = SocialAxis_UserDataManagement::validateWordpressSite($betaoutApiKey, $betaoutApiSecret, $wordpressVersion, $wordpressBoPluginUrl);
+        $curlResponse = Persona_UserDataManagement::validateWordpressSite($betaoutApiKey, $betaoutApiSecret, $wordpressVersion, $wordpressBoPluginUrl);
+
         if (isset($curlResponse['responseCode']) && $curlResponse['responseCode'] == 200) {
             $clientAccountName = $curlResponse['clientAccountName'];
+             $facebookEnable=isset($curlResponse['facebookConnected'])?$curlResponse['facebookConnected']:"N";
+             $twitterEnable=isset($curlResponse['twitterConnected'])?$curlResponse['twitterConnected']:"N";
+             $googleEnable=isset($curlResponse['googleConnected'])?$curlResponse['googleConnected']:"N";
+             $linkedinEnable=isset($curlResponse['linkedinConnected'])?$curlResponse['linkedinConnected']:"N";
             require_once('configuredSuccess.php');
             return;
         }
         else
             $showError = true;
     } catch (Exception $ex) {
-        
+
     }
 }
 ?>
 
 <div style="padding: 20px;">
-    <span>
-        <img src="<?php echo plugins_url('images/cloudSmall.png', dirname(dirname(__FILE__))); ?>" alt="" style="vertical-align: middle;margin-right: 20px;"/>
-        <span style="font:normal 24px Arial;color:#333333;">BetaOUT Configuration</span>
-    </span>
-    <div style="font: normal 13px Arial;color: #333333;">
-        <span>BetaOUT provides content and social infrastructure to your content network running on Wordpress.</span><br/>
-        <span>It seamlessly support multiple sites running on wordpress platform.</span>
+    <div style="float:left;width:100%">
+        <img src="<?php echo plugins_url('images/personaLogo.png', dirname(dirname(__FILE__))); ?>" alt="" style="float:left;margin-right: 10px;"/>
+        <span style="font:normal 24px Arial;color:#333333;float:left;margin-top: 5px;">Persona Configuration</span>
     </div>
-    <div style="background-color: #A9D5ED;display: inline-block;font: normal 13px Arial;color: #333333;padding: 5px;margin: 30px;">
-        <span>BetaOUT API Key for (<a href="<?php echo get_bloginfo('url'); ?>" style="font-style: italic;color: #797979;text-decoration: none"><?php echo get_bloginfo('url'); ?></a>)</span>
+    <div style="float: left;width: 100%;font: normal 13px Arial;color: #333333;margin-top: 20px;">
+        <span>Persona from BetaOUT provides social login, single sign on , social apps for your multiple Wordpress sites.</span><br/>
+        <span>This plugin will allow users to register with social networks like Twitter, Facebook and comment, share, rate easily.</span>
     </div>
+    <div style="float: left;width: 100%;">
+        <div style="background-color: #A9D5ED;font: normal 13px Arial;color: #333333;padding: 5px;margin: 30px 0 5px 30px;float: left">
+            <span>BetaOUT API Key for (<a href="<?php echo get_bloginfo('url'); ?>" style="font-style: italic;color: #797979;text-decoration: none"><?php echo get_bloginfo('url'); ?></a>)</span>
+        </div>
+    </div>
+
 
     <?php
     if ($showError) {
         ?>
-        <div style="min-height:25px;background-color:#f2dede;margin-left:30px;padding-top:5px;text-align: center;width:315px;-moz-border-radius:10px;border-radius:10px;" id="errorDiv">
-            <img id="errorDivImage" style="float:right;margin-right:3px;margin-top: -1px;cursor:pointer;" src="<?php echo plugins_url('images/closeIcon.png', dirname(dirname(__FILE__))); ?>"/> 
-            <span style="font-family: Arial;color:#b94a48;"><?php echo $curlResponse['error']; ?></span>
+        <div style="width: 100%;float: left">
+            <div style="min-height:25px;background-color:#f2dede;margin-left:30px;padding-top:5px;text-align: center;width:315px;-moz-border-radius:10px;border-radius:10px;" id="errorDiv">
+                <img id="errorDivImage" style="float:right;margin-right:3px;margin-top: -1px;cursor:pointer;" src="<?php echo plugins_url('images/closeIcon.png', dirname(dirname(__FILE__))); ?>"/>
+                <span style="font-family: Arial;color:#b94a48;"><?php echo $curlResponse['error']; ?></span>
+            </div>
         </div>
+
         <?php
     }
     ?>
 
 
-    <div style="margin: 0px 30px;">
-        <form action="" method="post" id="betaoutApiForm" style="width: 350px">
+    <div style="float: left;width: 100%">
+        <form action="" method="post" id="betaoutApiForm" style="width: 370px;margin-left: 30px">
             <fieldset>
                 <div class="control-group">
-                    <span>Key</span>
-                    <input type="text" value="<?php if($showError) echo get_option('_BETAOUT_API_KEY_TEMP', false); else echo get_option('_BETAOUT_API_KEY', false); ?>" id="betaoutApiKey" name="betaoutApiKey" class="inputText"/>
+                    <span>Site API Key</span>
+                    <input type="text" value="<?php if ($showError) echo get_option('_PERSONA_API_KEY_TEMP', false); else echo get_option('_PERSONA_API_KEY', false); ?>" id="betaoutApiKey" name="betaoutApiKey" class="inputText"/>
                 </div>
                 <div  class="control-group">
-                    <span>Secret</span>
-                    <input type="text" value="<?php if($showError) echo get_option('_BETAOUT_API_SECRET_TEMP', false); else echo get_option('_BETAOUT_API_SECRET', false); ?>" id="betaoutApiSecret" name="betaoutApiSecret" class="inputText"/>
+                    <span>Site API Secret</span>
+                    <input type="text" value="<?php if ($showError) echo get_option('_PERSONA_API_SECRET_TEMP', false); else echo get_option('_PERSONA_API_SECRET', false); ?>" id="betaoutApiSecret" name="betaoutApiSecret" class="inputText"/>
                 </div>
                 <div class="control-group">
-                    <input type="hidden" name="wordpressBoPluginUrl" id="wordpressBoPluginUrl" value="<?php echo plugins_url(); ?>/betaout"/>
+                    <input type="hidden" name="wordpressBoPluginUrl" id="wordpressBoPluginUrl" value="<?php echo plugins_url(); ?>/socialcloud"/>
                     <input type="hidden" name="wordpressVersion" id="wordpressVersion" value="<?php echo get_bloginfo('version'); ?>"/>
                     <input type="hidden" name="betaoutSubmit" value="submit"/>
                     <div style="float: right">
-                        <a href="http://access.betaout.com/admin-signup/nv/packageId/1" target="_blank">Get Your Free Key</a>
+                        <a href="http://access.betaout.com/persona/sign-up" target="_blank">Get Your Free Key</a>
                         <button type="button" onclick="javascript:submit();" class="buttonStyle">Save Key</button>
                     </div>
                 </div>
             </fieldset>
         </form>
+    </div>
+    <div style="float: left;width: 100%;font: normal 13px Arial;color: #333333;margin: 20px;">
+        <img src="<?php echo plugins_url('images/bulb.png', dirname(dirname(__FILE__))); ?>" alt="" style="float: left;margin-right: 10px"/>
+        <div style="float: left">
+            <span>To be able to use this plugin you first of all need to create a free account at <a href="http://access.betaout.com/persona/sign-up">http://access.betaout.com/persona/sign-up</a>  and setup a Site.</span>
+            <br/><span>After having created your account and adding your Site, get your Site API key and Secret.</span>
+        </div>
+
     </div>
 </div>
