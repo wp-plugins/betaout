67a83f73aa103bae3255c203827f0dd08411c6dd
Betaout <Betaout@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-24 06:24:27 +0000

    Gallery codes updated
    git-svn-id: https://plugins.svn.wordpress.org/betaout/tags/0.1.4@702656 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/BOGallery.php
+++ includes/BOGallery.php
@@ -10,12 +10,14 @@
 					</a>
 				</dt>
 			</dl>';
-		const galleryCss = ".custom-gallery{margin: auto}\n.custom-gallery .gallery-item{float: left;margin-top: 10px;text-align: center;width: 33%}\n.custom-gallery img{border: 2px solid #cfcfcf}\n.custom-gallery .gallery-caption{margin-left: 0}";
+		const galleryCss = ".custom-gallery{margin: auto}\n.custom-gallery .gallery-item{float: left;margin-top: 10px;text-align: center;width: 33%}\n.custom-gallery-title{padding:0 0 10px 0;font-size:18px};\n.custom-gallery-title a:hover{text-decoration:none};\n.custom-gallery img{border: 2px solid #cfcfcf}\n.custom-gallery .gallery-caption{margin-left: 0}";
 
 		private static $postGalleries = array();
+		private static $currentAssetPost = null;
+
 		public static function processGallery( $content ) {
 			global $post;
-			self::$postGalleries = @unserialize( get_post_meta( $post->ID, 'bo_assets', true ) );
+			self::$postGalleries = get_post_meta( $post->ID, 'bo_assets', true );
 
 			if( !is_array( self::$postGalleries ) || count( self::$postGalleries ) == 0 ) {
 				return $content;
@@ -23,12 +25,14 @@
 
 			$galleries = self::getComments( $content );
 			foreach( $galleries as $gallery ) {
-				if( strtolower( substr( trim( $gallery ), 0, 7 ) ) != 'gallery' ) {
+				if( strtolower( substr( trim( $gallery ), 0, 7 ) ) != 'gallery' && strtolower( substr( trim( $gallery ), 0, 9 ) ) != 'slideshow' ) {
 					continue;
 				}
 				$id = explode( ':', trim( $gallery ) );
-				if( isset( $id[ 1 ] ) && isset( self::$postGalleries[ $id[ 1 ] ] ) ) {				
-					if( $galleryHtml = self::getGalleryHtml( $id[ 1 ] ) ) {
+				if( isset( $id[ 1 ] ) && isset( self::$postGalleries[ $id[ 1 ] ] ) ) {
+					// clean_post_cache( self::$postGalleries[ $id[ 1 ] ][ 'wpId' ] );
+					self::$currentAssetPost = get_post( self::$postGalleries[ $id[ 1 ] ][ 'wpId' ] );
+					if( $galleryHtml = self::getGalleryHtml( self::$postGalleries[ $id[ 1 ] ][ 'wpId' ] ) ) {
 						$content = str_replace( '<!--' . $gallery . '-->', $galleryHtml, $content );
 					}
 				}
@@ -42,7 +46,7 @@
 
 		private static function getGalleryHtml( $galleryId ) {
 			$galleryImages = array();
-			$images = self::$postGalleries[ $galleryId ]['elements'];
+			$images = get_post_meta( $galleryId, 'bo_asset', true );
 			foreach( $images as $image ) {
 				$galleryImages[] = array(
 					'url' => $image[ 'view' ],
@@ -64,6 +68,7 @@
 					}
 				}
 				return '<div class="custom-gallery gallery gallery-columns-' . self::imageColumns . ' gallery-size-thumbnail">
+						<div class="custom-gallery-title"><a title="' . self::$currentAssetPost->post_title . '" href="' . get_permalink( self::$currentAssetPost->ID ) . '">' . self::$currentAssetPost->post_title . '</a></div>
 						' . $html . '
 						<br style="clear: both;" />
 					</div>';
